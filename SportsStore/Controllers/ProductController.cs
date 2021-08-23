f8f27d5a6e1b421404545a32a3a6e5e11fc3c929
Denis M <denis.m.a.3@gmail.com>
2021-08-23 21:49:39 +0300

    FirstCommit

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repository;
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
---
+++ SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product> 
+        { 
+            new Product { ProductID = 1, Category = "test", Description = "test2", Name = "Name1", Price = 1599 },
+            new Product { ProductID = 2, Category = "test", Description = "test3", Name = "Name2", Price = 25599 }
+        }
+        .AsQueryable<Product>();
+    }
+}
