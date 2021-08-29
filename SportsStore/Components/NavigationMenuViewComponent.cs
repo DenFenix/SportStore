964ce3d9f72073346cd1ea8db5e758fd37f24c07
Denis M <denis.m.a.3@gmail.com>
2021-08-30 00:40:35 +0300

    Починил работу категорий

--- SportsStore/Components/NavigationMenuViewComponent.cs
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -18,6 +18,7 @@ namespace SportsStore.Components
 
         public IViewComponentResult Invoke()
         {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
             return View(repository.Products
                 .Select(x=>x.Category)
                 .Distinct()
--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -21,7 +21,7 @@ namespace SportsStore.Controllers
             View(new ProductsListViewModel
             {
                 Products = _repository.Products
-                .Where(p=>p.Category==null||p.Category==category)
+                .Where(p=> category == null||p.Category==category)
                 .OrderBy(p => p.ProductID)
                 .Skip((productPage - 1) * PageSize)
                 .Take(PageSize),
